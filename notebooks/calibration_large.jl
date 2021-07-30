faaf1c0f8c94e7705f9eee7f018b4a985c2ef085
Dale Black <djblack@uci.edu>
2021-07-30 12:18:48 -0700

    update

--- notebooks/calibration_large.jl
+++ notebooks/calibration_large.jl
@@ -49,6 +49,9 @@ end
 # ╔═╡ 401e5509-40bf-48f8-8593-fd4f538c6805
 TableOfContents()
 
+# ╔═╡ 4f6cd82a-0fef-4f4b-bf89-c4628714bdf9
+thresh = 15
+
 # ╔═╡ baa7cd06-10e6-4f9a-8ffd-1f458253d5fb
 md"""
 ## Load data
@@ -61,10 +64,10 @@ md"""
 """
 
 # ╔═╡ 2fc032ad-b041-4fc4-8661-53b9fde5563d
-image_path = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 1^275\49\80.0";
+image_path = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 4^275\52\80.0";
 
 # ╔═╡ 639cd8f6-e16a-46dd-9430-8c38ce3648ae
-label_path = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 1^275\HEL_SLICER_SEG_0\80\L_5.0.nii";
+label_path = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 4^275\HEL_SLICER_SEG_0\80\L_5.0.nii";
 
 # ╔═╡ dc44351b-c4fb-44e3-918e-75a87dd3fb1a
 begin
@@ -78,23 +81,12 @@ NIfTI.orientation(lbl)
 # ╔═╡ 8b12e0d6-1285-4bca-bf79-0c629c1aec27
 img = dcmdir_parse(image_path);
 
-# ╔═╡ a2201222-dfb5-46d2-a40b-e36f390a2218
-md"""
-#### Reorient DICOM
-"""
-
-# ╔═╡ 1f4ab494-12e4-498b-b8fe-eabb68f9638d
-begin
-	aff = get_affine(img)
-	io = io_orientation(aff)
-	ornt2axcodes(io)
-end
-
 # ╔═╡ 7b6a3ca9-60ac-484e-a1a2-9c7e87c5719f
 orient = (("R", "P", "I"))
 
 # ╔═╡ 627fa140-9395-4470-bcea-54849cb61983
 begin
+	# Reorient
 	img_array = load_dcm_array(img)
 	img_array, affvol, new_affvol = DICOMUtils.orientation(img_array, orient)
 	img_array = permutedims(img_array, (2, 1, 3))
@@ -106,40 +98,55 @@ md"""
 """
 
 # ╔═╡ dc937793-ba7d-4e23-b55d-b73babaf68ec
-image_path2 = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 1^275\49\100.0";
-
-# ╔═╡ cf20f9dc-5776-4857-a746-acf216fe3391
-label_path2 = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 1^275\HEL_SLICER_SEG_0\100\L_5.0.nii";
-
-# ╔═╡ 8794f460-bc1b-4c74-9a2d-707fdc4acd3f
-begin
-	lbl_2 = niread(label_path2)
-	lbl_array2 = copy(lbl_2.raw)
-end;
-
-# ╔═╡ 58f323a6-92f9-4f7f-8969-5b71c5013dd7
-NIfTI.orientation(lbl_2)
+image_path2 = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 4^275\52\100.0";
 
 # ╔═╡ a3c0cb8a-2ce6-4a29-b371-f4f194973a41
 img_2 = dcmdir_parse(image_path2);
 
-# ╔═╡ 235fa3e9-c81e-4a1e-a4ac-25236a8f895d
-md"""
-#### Reorient DICOM
-"""
-
-# ╔═╡ 9e07d337-a792-4349-8d6d-56f21c68d639
-begin
-	aff2 = get_affine(img_2)
-	io2 = io_orientation(aff2)
-	ornt2axcodes(io2)
-end
-
 # ╔═╡ 6718792d-7dbc-4786-8be2-724db8458b5f
 begin
+	# Reorient
 	img_array2 = load_dcm_array(img_2)
 	img_array2, affvol2, new_affvol2 = DICOMUtils.orientation(img_array2, orient)
 	img_array2 = permutedims(img_array2, (2, 1, 3))
+end;
+
+# ╔═╡ 05d69d6e-f047-470a-92f4-550a01fd374f
+md"""
+### 120 kV
+"""
+
+# ╔═╡ 00ee0012-f44e-466c-a68e-0c5380436dd8
+image_path3 = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 4^275\52\120.0";
+
+# ╔═╡ dcf6b2cc-72c7-4322-b0ef-764d8a7590eb
+img_3 = dcmdir_parse(image_path3);
+
+# ╔═╡ 71b2bc03-ae83-4169-a26e-35addcb64770
+begin
+	# Reorient
+	img_array3 = load_dcm_array(img_3)
+	img_array3, affvol3, new_affvol3 = DICOMUtils.orientation(img_array3, orient)
+	img_array3 = permutedims(img_array3, (2, 1, 3))
+end;
+
+# ╔═╡ cf85c203-79f3-488b-965e-0a0b2d44a4c6
+md"""
+### 135 kV
+"""
+
+# ╔═╡ e114ac6a-375b-4f29-85ab-fe917f67137d
+image_path4 = raw"Y:\Canon Images for Dynamic Heart Phantom\Dynamic Phantom\clean_data\CONFIG 4^275\52\135.0";
+
+# ╔═╡ 94f63f2c-e1ca-4b9f-b65b-ab1066058e6d
+img_4 = dcmdir_parse(image_path4);
+
+# ╔═╡ f6afb134-3622-42cd-9a2e-1edda84cb4a9
+begin
+	# Reorient
+	img_array4 = load_dcm_array(img_4)
+	img_array4, affvol4, new_affvol4 = DICOMUtils.orientation(img_array4, orient)
+	img_array4 = permutedims(img_array4, (2, 1, 3))
 end;
 
 # ╔═╡ 3e804f4b-3b7d-47ac-ab58-0d3ba8662884
@@ -158,41 +165,23 @@ function collect_tuple(tuple_array)
 	return container
 end
 
-# ╔═╡ 0879fd99-58ed-4f93-b446-ca8190a91b85
-begin
-	l_indices = findall(x -> x == 1.0, lbl_array)
-	l_indices2 = findall(x -> x == 1.0, lbl_array2)
-end;
+# ╔═╡ 01f53372-5023-4da3-a085-af823755f4b5
+l_indices = findall(x -> x == 1.0, lbl_array);
 
-# ╔═╡ 5158b66a-9624-4cc1-aee9-bae91cc68c11
-begin
-	li = Tuple.(l_indices)
-	li2 = Tuple.(l_indices2)
-end;
+# ╔═╡ 388b30ad-02f2-47ca-a76e-debb954d26d5
+li = Tuple.(l_indices);
 
-# ╔═╡ 766b6bff-2244-4577-874a-f4d640f4f321
-begin
-	label_arr = collect_tuple(li)
-	label_arr2 = collect_tuple(li2)
-end;
+# ╔═╡ 8a3efe87-8931-4d97-a354-c4f6d0bdeebc
+label_arr = collect_tuple(li);
 
-# ╔═╡ ea4c93e9-80eb-4e6e-8dd3-6bc675698da0
-begin
-	zs_l = unique(label_arr[:,3])
-	zs_l2 = unique(label_arr2[:,3])
-end;
+# ╔═╡ 8ee2cb50-68c7-4efc-afec-a80b0ab698fb
+zs_l = unique(label_arr[:,3]);
 
 # ╔═╡ caa4ed3d-47e5-4030-bb9f-0d70f791f348
 @bind q PlutoUI.Slider(1:length(zs_l), default=10, show_value=true)
 
-# ╔═╡ a51aea16-bc50-4343-96d0-bba3e4333c11
-@bind r PlutoUI.Slider(1:length(zs_l2), default=10, show_value=true)
-
-# ╔═╡ 291bd287-2d82-4e1f-8325-66b7111b6b2f
-begin
-	indices_l = findall(x -> x == zs_l[q], label_arr[:,3])
-	indices_l2 = findall(x -> x == zs_l2[r], label_arr2[:,3])
-end;
+# ╔═╡ cb5ffd4d-c8fd-4b56-aa9c-5f331e18c116
+indices_l = findall(x -> x == zs_l[q], label_arr[:,3]);
 
 # ╔═╡ b4593c55-e98f-4357-b874-fb8dfa570e5c
 begin
@@ -201,17 +190,22 @@ begin
 	ax.title = "Large Insert (80 kV)"
 	heatmap!(ax, img_array[:,:,zs_l[q]], colormap=:grays)
 	scatter!(ax, label_arr[:,1][indices_l], label_arr[:,2][indices_l], markersize=1, color=:red)
-	fig
-end
-
-# ╔═╡ 923fe020-e24e-4770-9814-64582460173f
-begin
-	fig2 = Figure()
-	ax_2 = Makie.Axis(fig2[1, 1])
+	
+	ax_2 = Makie.Axis(fig[1, 2])
 	ax_2.title = "Large Insert (100 kV)"
-	heatmap!(ax_2, img_array2[:,:,zs_l2[r]], colormap=:grays)
-	scatter!(ax_2, label_arr2[:,1][indices_l2], label_arr2[:,2][indices_l2], markersize=1, color=:red)
-	fig2
+	heatmap!(ax_2, img_array2[:,:,zs_l[q]], colormap=:grays)
+	scatter!(ax_2, label_arr[:,1][indices_l], label_arr[:,2][indices_l], markersize=1, color=:red)
+	
+	ax_3 = Makie.Axis(fig[2, 1])
+	ax_3.title = "Large Insert (120 kV)"
+	heatmap!(ax_3, img_array3[:,:,zs_l[q]], colormap=:grays)
+	scatter!(ax_3, label_arr[:,1][indices_l], label_arr[:,2][indices_l], markersize=1, color=:red)
+	
+	ax_4 = Makie.Axis(fig[2, 2])
+	ax_4.title = "Large Insert (135 kV)"
+	heatmap!(ax_4, img_array3[:,:,zs_l[q]], colormap=:grays)
+	scatter!(ax_4, label_arr[:,1][indices_l], label_arr[:,2][indices_l], markersize=1, color=:red)
+	fig
 end
 
 # ╔═╡ 3cb21e2c-07e4-4e07-8331-5a0d48889072
@@ -226,13 +220,28 @@ md"""
 
 # ╔═╡ 77399f48-e100-45ea-ab0a-49b39e84f904
 begin
+	
+	ring = Bool.(dilate(dilate(dilate(dilate(lbl_array)))) - dilate(lbl_array))
+	
 	# 80 kV
-	ring = Bool.(dilate(lbl_array) - lbl_array)
-	S_BG = mean(img_array[ring])
+	r_img = img_array[ring]
+	r_img = r_img[r_img .> thresh]
+	S_BG = mean(r_img)
 	
 	# 100 kV
-	ring2 = Bool.(dilate(lbl_array2) - lbl_array2)
-	S_BG2 = mean(img_array2[ring2])
+	r_img2 = img_array2[ring]
+	r_img2 = r_img2[r_img2 .> thresh]
+	S_BG2 = mean(r_img2)
+	
+	# 120 kV
+	r_img3 = img_array3[ring]
+	r_img3 = r_img3[r_img3 .> thresh]
+	S_BG3 = mean(r_img3)
+	
+	# 135 kV
+	r_img4 = img_array4[ring]
+	r_img4 = r_img4[r_img4 .> thresh]
+	S_BG4 = mean(r_img4)
 end
 
 # ╔═╡ bad81568-5edc-4821-a0cb-6833f911ac0b
@@ -242,15 +251,28 @@ md"""
 
 # ╔═╡ f1361ff7-f0b5-4c91-9dfd-63474ce808ab
 begin
+	
+	core = Bool.((erode(erode(lbl_array))))
+	
 	# 80 kV
-	core = Bool.(erode(lbl_array))
-	core = erode(core)
-	S_O = mean(img_array[core])
+	c_img = img_array[core]
+	c_img = c_img[c_img .> thresh]
+	S_O = mean(c_img)
 	
 	# 100 kV
-	core2 = Bool.(erode(lbl_array2))
-	core2 = erode(core2)
-	S_O2 = mean(img_array2[core2])
+	c_img2 = img_array2[core]
+	c_img2 = c_img2[c_img2 .> thresh]
+	S_O2 = mean(c_img2)
+	
+	# 120 kV
+	c_img3 = img_array3[core]
+	c_img3 = c_img3[c_img3 .> thresh]
+	S_O3 = mean(c_img3)
+	
+	# 135 kV
+	c_img4 = img_array4[core]
+	c_img4 = c_img4[c_img4 .> thresh]
+	S_O4 = mean(c_img4)
 end
 
 # ╔═╡ 9d56d1bf-171d-4e66-a0f3-f610dfde9edd
@@ -266,28 +288,12 @@ begin
 	f = Figure()
 	ax1 = Makie.Axis(f[1, 1])
 	ax1.title = "Ring 80 kV (S_BG)"
-	heatmap!(ax1, ring[:, :, b])
+	heatmap!(ax1, ring[:, :, b], colormap=:grays)
 	
 	ax2 = Makie.Axis(f[1, 2])
 	ax2.title = "Core 80 kV (S_O)"
-	heatmap!(ax2, core[:, :, b])
+	heatmap!(ax2, core[:, :, b], colormap=:grays)
 	f
-end
-
-# ╔═╡ 678f6770-b606-470d-96f3-085246456f42
-@bind c PlutoUI.Slider(1:size(lbl_array2)[3]; default=215, show_value=true)
-
-# ╔═╡ 572941be-0738-4c78-9191-855b477d2755
-begin
-	f2 = Figure()
-	ax1_2 = Makie.Axis(f2[1, 1])
-	ax1_2.title = "Ring 100 kV (S_BG)"
-	heatmap!(ax1_2, ring2[:, :, c])
-	
-	ax2_2 = Makie.Axis(f2[1, 2])
-	ax2_2.title = "Core 100 kV (S_O)"
-	heatmap!(ax2_2, core2[:, :, c])
-	f2
 end
 
 # ╔═╡ 6e52d101-f7ac-4063-8e69-4562854639dc
@@ -296,7 +302,7 @@ md"""
 """
 
 # ╔═╡ f21103e4-f3ad-44ac-b877-4c95e8000a72
-S_BGs, S_Os = [S_BG, S_BG2], [S_O, S_O2]
+S_BGs, S_Os = [S_BG, S_BG2, S_BG3, S_BG4], [S_O, S_O2, S_O3, S_O4]
 
 # ╔═╡ 5ef0ed13-59d6-44cb-949f-e92e26a7662b
 df = DataFrame(S_BGs = S_BGs, S_Os = S_Os)
@@ -310,37 +316,37 @@ df = DataFrame(S_BGs = S_BGs, S_Os = S_Os)
 # ╔═╡ Cell order:
 # ╠═85e8a0e3-4de3-4933-9a52-35436bbde55f
 # ╠═401e5509-40bf-48f8-8593-fd4f538c6805
+# ╠═4f6cd82a-0fef-4f4b-bf89-c4628714bdf9
 # ╟─baa7cd06-10e6-4f9a-8ffd-1f458253d5fb
-# ╟─06db439a-134e-4484-8f10-9102cfef5352
 # ╠═2fc032ad-b041-4fc4-8661-53b9fde5563d
+# ╠═dc937793-ba7d-4e23-b55d-b73babaf68ec
+# ╠═00ee0012-f44e-466c-a68e-0c5380436dd8
+# ╠═e114ac6a-375b-4f29-85ab-fe917f67137d
 # ╠═639cd8f6-e16a-46dd-9430-8c38ce3648ae
+# ╟─06db439a-134e-4484-8f10-9102cfef5352
 # ╠═dc44351b-c4fb-44e3-918e-75a87dd3fb1a
 # ╠═6ebd6d62-c26b-4c72-ad90-f109f4da35f5
 # ╠═8b12e0d6-1285-4bca-bf79-0c629c1aec27
-# ╟─a2201222-dfb5-46d2-a40b-e36f390a2218
-# ╠═1f4ab494-12e4-498b-b8fe-eabb68f9638d
 # ╠═7b6a3ca9-60ac-484e-a1a2-9c7e87c5719f
 # ╠═627fa140-9395-4470-bcea-54849cb61983
 # ╟─dc2e2b3f-77e9-4845-a6e5-65547a49dd70
-# ╠═dc937793-ba7d-4e23-b55d-b73babaf68ec
-# ╠═cf20f9dc-5776-4857-a746-acf216fe3391
-# ╠═8794f460-bc1b-4c74-9a2d-707fdc4acd3f
-# ╠═58f323a6-92f9-4f7f-8969-5b71c5013dd7
 # ╠═a3c0cb8a-2ce6-4a29-b371-f4f194973a41
-# ╟─235fa3e9-c81e-4a1e-a4ac-25236a8f895d
-# ╠═9e07d337-a792-4349-8d6d-56f21c68d639
 # ╠═6718792d-7dbc-4786-8be2-724db8458b5f
+# ╟─05d69d6e-f047-470a-92f4-550a01fd374f
+# ╠═dcf6b2cc-72c7-4322-b0ef-764d8a7590eb
+# ╠═71b2bc03-ae83-4169-a26e-35addcb64770
+# ╟─cf85c203-79f3-488b-965e-0a0b2d44a4c6
+# ╠═94f63f2c-e1ca-4b9f-b65b-ab1066058e6d
+# ╠═f6afb134-3622-42cd-9a2e-1edda84cb4a9
 # ╟─3e804f4b-3b7d-47ac-ab58-0d3ba8662884
 # ╠═9ce79ba5-4b57-4cef-b809-90f1c01f2e3e
-# ╠═0879fd99-58ed-4f93-b446-ca8190a91b85
-# ╠═5158b66a-9624-4cc1-aee9-bae91cc68c11
-# ╠═766b6bff-2244-4577-874a-f4d640f4f321
-# ╠═ea4c93e9-80eb-4e6e-8dd3-6bc675698da0
-# ╠═291bd287-2d82-4e1f-8325-66b7111b6b2f
+# ╠═01f53372-5023-4da3-a085-af823755f4b5
+# ╠═cb5ffd4d-c8fd-4b56-aa9c-5f331e18c116
+# ╠═388b30ad-02f2-47ca-a76e-debb954d26d5
+# ╠═8a3efe87-8931-4d97-a354-c4f6d0bdeebc
+# ╠═8ee2cb50-68c7-4efc-afec-a80b0ab698fb
 # ╟─caa4ed3d-47e5-4030-bb9f-0d70f791f348
-# ╟─b4593c55-e98f-4357-b874-fb8dfa570e5c
-# ╟─a51aea16-bc50-4343-96d0-bba3e4333c11
-# ╟─923fe020-e24e-4770-9814-64582460173f
+# ╠═b4593c55-e98f-4357-b874-fb8dfa570e5c
 # ╟─3cb21e2c-07e4-4e07-8331-5a0d48889072
 # ╟─313ad781-97b0-4091-bb1a-a3534ee00b2d
 # ╠═77399f48-e100-45ea-ab0a-49b39e84f904
@@ -348,9 +354,7 @@ df = DataFrame(S_BGs = S_BGs, S_Os = S_Os)
 # ╠═f1361ff7-f0b5-4c91-9dfd-63474ce808ab
 # ╟─9d56d1bf-171d-4e66-a0f3-f610dfde9edd
 # ╟─105dbd69-b579-41ed-a3ce-0ea4124c547b
-# ╟─019d3f86-3ee8-4615-8a62-69a93f93d5ca
-# ╟─678f6770-b606-470d-96f3-085246456f42
-# ╟─572941be-0738-4c78-9191-855b477d2755
+# ╠═019d3f86-3ee8-4615-8a62-69a93f93d5ca
 # ╟─6e52d101-f7ac-4063-8e69-4562854639dc
 # ╠═f21103e4-f3ad-44ac-b877-4c95e8000a72
 # ╠═5ef0ed13-59d6-44cb-949f-e92e26a7662b
